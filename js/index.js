6c2d68ba789d905349f55d5d723f2db3120f1f9f
Umka1993 <58582062+Umka1993@users.noreply.github.com>
2021-06-18 18:04:29 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,34 +1,96 @@
-// Скрипт slick (слайдер)
-$(".slider-block").slick({
-    dots: true,
-    autoplay: false,
-    infinite: true,
-    arrow: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    prevArrow: '<a class="your-class-btn-back">Back</a>',
-    nextArrow: '<a class="your-class-btn-forward">Forward</a>',
-    responsive: [{
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                centerMode: false,
+// Скрипт swiper-slider
+new Swiper(".testimonials__slider", {
+    //стрелки
+    navigation: {
+        nextEl: "swiper-button-next",
+        prevEl: "swiper-button-prev",
+    },
+    // Дотсы
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    //изменение  курсора
+    grabCursor: true,
 
-            },
+    //пеертаскивание с меньшим усилием
+    tochRatio: 1,
+
+    //скролл с помощью колеса  мыши
+    // mousewheel: {
+
+    //чувствительность колеса мыши
+    // sensitivity: 1,
+    // },
+
+    //автовысота (на случай текста разного объема)
+    autoHeight: true,
+
+    //колличество слайдов при показе
+    slidesPerView: 2.5,
+
+    //если слайдов меньше чем нужно, например только 1, то слайдер отключится и этот слайд будет оказыватсья просто ввиде контента
+    watchOverflow: true,
+
+    //отступ между слайдами
+    spaceBetween: 177.5,
+
+    //кол-во пролистываемых слайдов
+    slidesPerGroup: 1,
+
+    //активный слайд по центру
+    centeredSlides: true,
+
+    //стартовый слайд
+    // initialSlide: 1,
+
+    //бесконечная прокрутка
+    loop: true,
+
+    //свободный режим (нужен для плавного скрола и для плавного перемещения слайдов мышью)
+    freeMode: true,
+
+    speed: 800,
+    //автопрокрутка  слайдов
+    // autoplay: {
+    //пауза между прокруткой
+    // delay: 1000,
+
+    //закончить на последнем слайде
+    // stopOnLastSlide: true,
+
+    //отключить после ручного переключения
+    // disableOnInteraction: true,
+    // }
+
+    //сделать слайдер вертикальным
+    // direction: 'vertical',
+
+    breakpoints: {
+        320: {
+            //колличество слайдов при показе
+            slidesPerView: 1,
         },
-        {
-            breakpoint: 375,
-            settings: {
-                slidesToShow: 1,
-                centerMode: false,
-                slidesToScroll: 1,
-                arrows: false,
-            },
+        480: {
+            //колличество слайдов при показе
+            slidesPerView: 1,
         },
-    ],
+        767: {
+            //колличество слайдов при показе
+            slidesPerView: 1,
+        },
+
+        768: {
+            //колличество слайдов при показе
+            slidesPerView: 1,
+            spaceBetween: 100,
+        },
+        992: {
+            //колличество слайдов при показе
+            slidesPerView: 2,
+            spaceBetween: 100,
+        },
+    },
 });
 
 // скрипт для модалки
@@ -84,3 +146,21 @@ function closeQuoteWindow() {
     modal.classList.remove("open__modal");
     body.classList.remove("wrapper");
 }
+
+// Плавный скролл
+// document.querySelectorAll('a[href^="#"').forEach((link) => {
+//     link.addEventListener("click", function(e) {
+//         e.preventDefault();
+
+//         let href = this.getAttribute("href").substring(1);
+//         const scrollTarget = document.getElementById(href);
+//         const topOffset = 0;
+//         const elementPosition = scrollTarget.getBoundingClientRect().top;
+//         const offsetPosition = elementPosition - topOffset;
+
+//         window.scrollBy({
+//             top: offsetPosition,
+//             behavior: "smooth",
+//         });
+//     });
+// });
